bf7778be588b988db7c14ede44d6fe18980100dc
charliech17 <charliech17@yahoo.com.tw>
2021-11-04 17:13:17 +0800

    modify website to be responsive

--- mr_frontend/js/allMessage.js
+++ mr_frontend/js/allMessage.js
@@ -5,7 +5,7 @@ import { message } from "./fetch_message.js";
 message.then((msg) => {
   for (let i = 0; i <= 9; i++) {
     let text = msg[i][0];
-    let a = $('<a/>').text(`W${msg[i][1]}` + text.substring(text.indexOf("\n")))
+    let a = $('<a/>').text(`M${msg[i][1]}` + text.substring(text.indexOf("\n")))
                    .attr('href','selectedMessage.html').attr('data-ith',i);
     $(`.all_article .art_${i + 1}`).append(a);
   }
